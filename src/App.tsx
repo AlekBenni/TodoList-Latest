2c650c86fd388aa5fa9198ad710bc473a54de8f0
Alek Benny <adeyva@mail.ru>
2021-02-04 20:34:32 +0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect } from 'react';
-import {Todolist, TaskType} from './Todolist';
+import { Todolist, TaskType } from './Todolist';
 import './App.css'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -13,11 +13,11 @@ import Grid from '@material-ui/core/Grid';
 import { addTodolistTC, fetchTodolistThunkTC } from './state/todolist-reducer';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootStateType } from './state/store';
-import {GetTodolists, CreateTodolist, DeleteTodolist, UpdateTodolistTitle, GetTasks, DeleteTask, CreateTask} from './testAPI'
-import {TaskStatuses} from './API/todolist-api'
+import { GetTodolists, CreateTodolist, DeleteTodolist, UpdateTodolistTitle, GetTasks, DeleteTask, CreateTask } from './testAPI'
+import { TaskStatuses } from './API/todolist-api'
 import LinearProgress from '@material-ui/core/LinearProgress'
-import {ErrorSnackbar} from './ErrorSnackBar';
-import {initAppTC, RequestStatusType} from './state/app-reducer'
+import { ErrorSnackbar } from './ErrorSnackBar';
+import { initAppTC, RequestStatusType } from './state/app-reducer'
 import { BrowserRouter, Route } from 'react-router-dom';
 import TodolistList from './TodolistList';
 import { Login } from './Login/Login';
@@ -26,13 +26,13 @@ import { logoutTC } from './Login/login-reducer';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      flexGrow: 1,
+        flexGrow: 1,
     },
     menuButton: {
-      marginRight: theme.spacing(2),
+        marginRight: theme.spacing(2),
     },
     title: {
-      flexGrow: 1,
+        flexGrow: 1,
     },
     contStyle: {
         padding: '25px'
@@ -47,7 +47,7 @@ const useStyles = makeStyles((theme) => ({
         height: '100vh',
         alignItems: 'center'
     }
-  }));
+}));
 
 export type FilterValuesType = "all" | "active" | "completed"
 
@@ -55,16 +55,16 @@ export type TodolistType = {
     id: string
     title: string
     filter: FilterValuesType
-    entityStatus? :string
+    entityStatus?: string
 }
 
 export type TaskStateType = {
-    [key:string]: Array<TaskType>
+    [key: string]: Array<TaskType>
 }
 
 function App() {
     const classes = useStyles();
-    
+
     const preLoader = useSelector<RootStateType, RequestStatusType>((state) => state.app.status)
     const initApp = useSelector<RootStateType, boolean>((state) => state.app.isAuth)
     const isLoginIn = useSelector<RootStateType, boolean>((state) => state.login.isLoginIn)
@@ -72,39 +72,39 @@ function App() {
 
     useEffect(() => {
         dispatch(initAppTC())
-    },[])   
+    }, [])
 
     const logout = useCallback(() => {
         dispatch(logoutTC())
     }, [])
 
-    if(!initApp){return <div className={classes.progress}><CircularProgress color="secondary" /></div> }
-    
+    if (!initApp) { return <div className={classes.progress}><CircularProgress color="secondary" /></div> }
+
     const demo = false
 
     return (
         <BrowserRouter>
-        <div className="App">
-            <AppBar position="static" color="secondary">
-                <Toolbar>
-                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" className={classes.title}> Todolistss </Typography>
-                {isLoginIn === true && <Button onClick={logout} color="inherit">Logout</Button>}
-                
-                </Toolbar>
-            </AppBar>
-            {preLoader === 'loading' && <LinearProgress color="secondary" /> }
-            <Container fixed>
+            <div className="App">
+                <AppBar position="static" color="secondary">
+                    <Toolbar>
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" className={classes.title}> Todolists </Typography>
+                        {isLoginIn === true && <Button onClick={logout} color="inherit">Logout</Button>}
 
-            <Route exact path = {"/"} render = {() => <TodolistList demo={demo}/>} />
-            <Route path = {"/login"} render = {() => <Login />} />
-  
-            <ErrorSnackbar />
+                    </Toolbar>
+                </AppBar>
+                {preLoader === 'loading' && <LinearProgress color="secondary" />}
+                <Container fixed>
 
-            </Container>
-        </div>
+                    <Route exact path={"/"} render={() => <TodolistList demo={demo} />} />
+                    <Route path={"/login"} render={() => <Login />} />
+
+                    <ErrorSnackbar />
+
+                </Container>
+            </div>
         </BrowserRouter>
     );
 }
